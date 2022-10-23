67d334cd4bd47463b9018f0974c0cef3af6947fe
NodeJS <deneme@deneme.com>
2022-10-23 19:21:34 +0200

    revize

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.use('/', (req, res) => {
-    res.send('Deneme');
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
